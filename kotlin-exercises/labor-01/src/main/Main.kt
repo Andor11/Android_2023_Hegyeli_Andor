1a9c49ba630aa8a08cc2c40e5bf94c120cb00337
Andor11 <99787492+Andor11@users.noreply.github.com>
2023-09-29 08:39:23 +0300

    First test ignore

--- kotlin-exercises/labor-01/src/main/Main.kt
+++ kotlin-exercises/labor-01/src/main/Main.kt
@@ -2,6 +2,7 @@ package main
 
 fun main(args: Array<String>) {
     println("Hello World!")
+    println("Teszt");
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
