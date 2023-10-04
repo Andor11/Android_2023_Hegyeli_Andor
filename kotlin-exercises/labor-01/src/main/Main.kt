399c08029707375bd4005c26cd8f505ef66e16fc
Andor11 <99787492+Andor11@users.noreply.github.com>
2023-10-04 13:20:20 +0300

    All task done

--- kotlin-exercises/labor-01/src/main/Main.kt
+++ kotlin-exercises/labor-01/src/main/Main.kt
@@ -1,5 +1,7 @@
 package main
 
+import kotlin.random.Random
+
 fun main(args: Array<String>) {
     //println("Hello World!")
     //println("Teszt");
@@ -22,11 +24,9 @@ fun main(args: Array<String>) {
     printTheWeek(daysOfWeek);
 
     println(" ");
-    println("Days with T start: ");
     printDayStarWithT(daysOfWeek);
 
     println(" ");
-    println("Days contain e letter: ");
     printDayContainsLetterLittleE(daysOfWeek);
 
     println("");
@@ -40,6 +40,88 @@ fun main(args: Array<String>) {
     var start = 5;
     var end = 15;
     printPrimeNumberInRange(start,end);
+    println("----------");
+    println(" ");
+
+    println("Task 4: Write an encode and a corresponding decode function that encodes and respectively\n" +
+            "decodes the characters of a string.\n");
+
+    val message = "Hello, World!"
+    val shift = 3
+
+    val encodedMessage = messageCoding(message, ::encode, shift)
+    val decodedMessage = messageCoding(encodedMessage, ::decode, shift)
+
+    println("Original Message: $message")
+    println("Encoded Message: $encodedMessage")
+    println("Decoded Message: $decodedMessage")
+    println("----------");
+    println(" ");
+
+    println("Task 5: Write a compact function that prints the even numbers from a list. Use a list filter!\n")
+    val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    printEvenNumbers(numbers);
+    println("----------");
+    println(" ");
+
+    println("Task 6: The map() performs the same transformation on every list item and returns the result list.\n");
+    val doubledNumbers = numbers.map { it * 2 }
+
+    println("Original list: $numbers")
+    println("List with doubled elements: $doubledNumbers")
+
+    val capitalizedDaysOfWeek = daysOfWeek.map { it.toUpperCase() }
+    println("Capitalized days of week: $capitalizedDaysOfWeek");
+
+    val firstCharsCapitalized = daysOfWeek.map { it.firstOrNull()?.toLowerCase() }
+    println("First letters: $firstCharsCapitalized");
+
+    val lengthOfDays = daysOfWeek.map { it.length };
+    println("Length of days: $lengthOfDays");
+
+    val averageLength = lengthOfDays.sum().toDouble() / daysOfWeek.size;
+    println("Avg. length of days: $averageLength")
+    println("----------");
+    println(" ");
+
+    println("Task 7: Mutable list\n")
+
+    val mutableDaysOfWeek = daysOfWeek.toMutableList()
+    mutableDaysOfWeek.removeIf { it.contains('n') }
+    println("Mutable list after removing days with 'n': $mutableDaysOfWeek")
+    for ((index, item) in mutableDaysOfWeek.withIndex()) {
+        println("Item at $index is $item")
+    }
+    mutableDaysOfWeek.sort();
+    println("Sorted list: $mutableDaysOfWeek");
+
+    println("----------");
+    println(" ");
+
+    println("Task 8: Arrays\n");
+
+    println("Array of 10 with 0-100 randoms: ")
+    val random = Random.Default
+    val randomIntegers = IntArray(10) { random.nextInt(101) } // Generate 10 random integers between 0 and 100
+    randomIntegers.forEach { println(it) }
+    val ascending = randomIntegers.sorted();
+    println("Ascending sorted lis: $ascending")
+
+    if (containsEvenNumber(randomIntegers)){
+        println("Contains even(s)");
+    } else {
+        println("Doesn't contains even(s)");
+    }
+    if (allEvenNumber(randomIntegers)){
+        println("All evens");
+    } else {
+        println("Not all evens");
+    }
+
+    val avgOfRandoms = randomIntegers.sum().toDouble() / randomIntegers.size;
+    println("Generated numbers:")
+    randomIntegers.forEach { println(it) }
+    println("Average of the list: $avgOfRandoms");
 }
 
 fun printPrimeNumberInRange(start: Int, end: Int) {
@@ -61,12 +143,6 @@ fun printPrimeNumberInRange(start: Int, end: Int) {
     }
 }
 
-fun codeString(input: String): String {
-    var result: String = input;
-   // result = result <<;
-
-    return result;
-}
 fun isPrime(number: Int): Boolean {
     if (number <= 1) {
         return false
@@ -95,19 +171,23 @@ fun printDayNameWithLenght6(daysOfWeek: List<String>) {
 }
 
 fun printDayContainsLetterLittleE(daysOfWeek: List<String>) {
-    for ((index, item) in daysOfWeek.withIndex()) {
-        when {
-            "e" in item -> println("${index+1}. $item")
-        }
-    }
+//    for ((index, item) in daysOfWeek.withIndex()) {
+//        when {
+//            "e" in item -> println("${index+1}. $item")
+//        }
+//    }
+    val daysContainingE = daysOfWeek.filter { it.contains('e') }
+    println("Days containing 'e': $daysContainingE")
 }
 
 fun printDayStarWithT(daysOfWeek: List<String>) {
-    for ((index, item) in daysOfWeek.withIndex()) {
-        when {
-            "T" in item -> println("${index+1}. $item")
-        }
-    }
+//    for ((index, item) in daysOfWeek.withIndex()) {
+//        when {
+//            "T" in item -> println("${index+1}. $item")
+//        }
+//    }
+    val daysStartingWithT = daysOfWeek.filter { it.startsWith('T') }
+    println("Days starting with 'T': $daysStartingWithT")
 }
 
 fun addTwoNumbers (number1: Number, number2: Number){
@@ -116,4 +196,38 @@ fun addTwoNumbers (number1: Number, number2: Number){
 }
 fun printTheWeek(daysOfWeek: List<String>) {
     for ((index, item) in daysOfWeek.withIndex()) println("${index+1}. $item")
+}
+
+fun encode(input: String, shift: Int): String {
+    return input.map { char ->
+        if (char.isLetter()) {
+            val isLowerCase = char.isLowerCase()
+            val base = if (isLowerCase) 'a' else 'A'
+            ((char.toInt() - base.toInt() + shift) % 26 + base.toInt()).toChar()
+        } else {
+            char
+        }
+    }.joinToString("")
+}
+
+fun decode(input: String, shift: Int): String {
+    return encode(input, -shift)
+}
+
+fun messageCoding(msg: String, func: (String, Int) -> String, shift: Int): String {
+    return func(msg, shift)
+}
+
+fun printEvenNumbers(numbers: List<Int>) {
+    println("Even numbers:");
+    val result = numbers.filter { it % 2 == 0 }.forEach { println(it) }
+
+}
+
+fun containsEvenNumber(arr: IntArray): Boolean {
+    return arr.any { it % 2 == 0 }
+}
+
+fun allEvenNumber(arr: IntArray): Boolean {
+    return arr.all { it % 2 == 0 }
 }
